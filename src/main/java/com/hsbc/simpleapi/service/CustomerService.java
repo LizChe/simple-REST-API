f426b30c6a65c43f707e5c95b3b115798d5c6c2f
Liz <pachaffy@gmail.com>
2019-09-24 21:53:22 +0200

    Implement create customer method in CustomerService

--- src/main/java/com/hsbc/simpleapi/service/CustomerService.java
+++ src/main/java/com/hsbc/simpleapi/service/CustomerService.java
@@ -1,4 +1,24 @@
 package com.hsbc.simpleapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hsbc.simpleapi.model.Customer;
+import com.hsbc.simpleapi.dao.CustomerRepository;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
 public class CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer insert(Customer customer) {
+        return customerRepository.save(customer);
+    }
 }
