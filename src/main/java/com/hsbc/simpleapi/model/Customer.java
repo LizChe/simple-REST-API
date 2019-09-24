cea3075d780654c8f7c6ec12a13ff72d0a8d9437
Liz <pachaffy@gmail.com>
2019-09-24 18:49:32 +0200

    Implement Customer.java model

--- src/main/java/com/hsbc/simpleapi/model/Customer.java
+++ src/main/java/com/hsbc/simpleapi/model/Customer.java
@@ -1,4 +1,32 @@
 package com.hsbc.simpleapi.model;
 
 public class Customer {
+
+    private long id;
+    private String name;
+    private Address address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
